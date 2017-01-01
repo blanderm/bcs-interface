993d7cc57795405410e6afaa61e1376a53636fbd
Bryan Landerman <bryan@dealer.com>
2017-01-01 15:02:40 -0500

    added timer formatting

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -48,19 +48,30 @@ angular.module('hopologybrewing-bcs', [])
                                     }
                                 }
 
-                                $scope.convertTimerValue = function(value) {
+                                $scope.convertTimerValue = function (value) {
+                                    // days
+                                    var calculatedTimer = value / 10 / 60 / 60 / 24;
+                                    var strTimer = "";
+
+                                    if (calculatedTimer > 1) {
+                                        strTimer = Math.floor(calculatedTimer) + " days ";
+                                    }
+
                                     // hours
-                                    var calculatedTimer = value/10/60/60;
-                                    var strTimer = "" + Math.floor(calculatedTimer) + ":";
+                                    calculatedTimer = calculatedTimer % 1 * 24;
+                                    var floor = Math.floor(calculatedTimer);
+                                    strTimer = strTimer + (floor < 10 ? '0' + floor : floor)  + ":";
 
                                     // mins
-                                    calculatedTimer = calculatedTimer%1*60;
-                                    strTimer = strTimer + Math.floor(calculatedTimer) + ":";
+                                    calculatedTimer = calculatedTimer % 1 * 60;
+                                    floor = Math.floor(calculatedTimer);
+                                    strTimer = strTimer + (floor < 10 ? '0' + floor : floor)  + ":";
 
                                     // seconds
-                                    calculatedTimer = calculatedTimer%1*60;
+                                    calculatedTimer = calculatedTimer % 1 * 60;
+                                    floor = Math.floor(calculatedTimer);
 
-                                    return strTimer + Math.floor(calculatedTimer);
+                                    return strTimer + (floor < 10 ? '0' + floor : floor) ;
                                 };
 
                                 //$scope.activeStateTimers = stateResponse.data.timers;
@@ -70,8 +81,119 @@ angular.module('hopologybrewing-bcs', [])
             });
     })
 
-    .
-    controller('chartController', function ($scope, $http) {
+    .controller('gaugeController', function ($scope, $http) {
+        $http.get('/temp').
+            then(function (response) {
+                // find active process and get current state
+                if (response.data != null) {
+                    $scope.tempProbes = response.data;
+                    console.log($scope.tempProbes);
+                }
+            });
+
+
+        $scope.getGauge = function (probeId, divId) {
+            $http.get('/temp/' + probeId).
+                then(function (response) {
+                    Highcharts.chart(divId + probeId, {
+                            chart: {
+                                type: 'gauge',
+                                plotBackgroundColor: null,
+                                plotBackgroundImage: null,
+                                plotBorderWidth: 0,
+                                plotShadow: false
+                            },
+
+                            exporting: {
+                                enabled: false
+                            },
+
+                            title: {
+                                text: response.data[0].name + "<br>(" + response.data[0].setpoint + " SP)"
+                            },
+
+                            pane: {
+                                startAngle: -150,
+                                endAngle: 150,
+                                background: [{
+                                    backgroundColor: {
+                                        linearGradient: {x1: 0, y1: 0, x2: 0, y2: 1},
+                                        stops: [
+                                            [0, '#FFF'],
+                                            [1, '#333']
+                                        ]
+                                    },
+                                    borderWidth: 0,
+                                    outerRadius: '109%'
+                                }, {
+                                    backgroundColor: {
+                                        linearGradient: {x1: 0, y1: 0, x2: 0, y2: 1},
+                                        stops: [
+                                            [0, '#333'],
+                                            [1, '#FFF']
+                                        ]
+                                    },
+                                    borderWidth: 1,
+                                    outerRadius: '107%'
+                                }, {
+                                    // default background
+                                }, {
+                                    backgroundColor: '#DDD',
+                                    borderWidth: 0,
+                                    outerRadius: '105%',
+                                    innerRadius: '103%'
+                                }]
+                            },
+
+                            // the value axis
+                            yAxis: {
+                                min: 35,
+                                max: 80,
+
+                                minorTickInterval: 'auto',
+                                minorTickWidth: 1,
+                                minorTickLength: 10,
+                                minorTickPosition: 'inside',
+                                minorTickColor: '#666',
+
+                                tickPixelInterval: 30,
+                                tickWidth: 2,
+                                tickPosition: 'inside',
+                                tickLength: 10,
+                                tickColor: '#666',
+                                labels: {
+                                    step: 2,
+                                    rotation: 'auto'
+                                },
+                                plotBands: [{
+                                    from: 65,
+                                    to: 70,
+                                    color: '#606060'
+                                },
+                                    {
+                                        from: ((response.data[0].setpoint > 0) ? (response.data[0].setpoint - 0.25) : 68),
+                                        to: ((response.data[0].setpoint > 0) ? (response.data[0].setpoint + 0.25) : 68),
+                                        color: ((response.data[0].setpoint > 0) ? '#55BF3B' : '#606060')
+                                    }]
+                            },
+                            series: response.data
+                        },
+                        function (chart) {
+                            if (!chart.renderer.forExport) {
+                                setInterval(function () {
+                                    var point = chart.series[0].points[0];
+                                    $.getJSON('/temp/' + probeId, function (data) {
+                                        point.update(data[0].data[0]);
+                                    });
+                                }, 300000);
+                            }
+                        }
+                    )
+                });
+        };
+    })
+
+    .controller('chartController', function ($scope, $http) {
         $http.get('/temp/history').
             then(function (response) {
                 Highcharts.chart('temp-history', {
